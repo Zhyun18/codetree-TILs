8cf8b3fb90ad2d3ce3106b0b585dcb52158f8941
Zhyun18 <118049891+Zhyun18@users.noreply.github.com>
2024-01-31 01:48:22 +0900

    Title: 비교 연산, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240131/비교 연산/comparison-operator.py
@@ -0,0 +1,31 @@
+a,b=input().split()
+a,b=int(a),int(b)
+if a>=b:
+    print(1)
+else:
+    print(0)
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if b>=a:
+    print(1)
+else:
+    print(0)
+
+if b>a:
+    print(1)
+else:
+    print(0)
+
+if a==b:
+    print(1)
+else:
+    print(0)
+
+if a!=b:
+    print(1)
+else:
+    print(0)
