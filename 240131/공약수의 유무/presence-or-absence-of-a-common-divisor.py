0848e9acbd66f6a5dff9bfbcd7df3982d77668bb
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-01-31 21:33:22 +0900

    Title: 공약수의 유무, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240131/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -2,11 +2,15 @@
 a,b=input().split()
 a,b=int(a),int(b)
 
+# 존재유무를 저장할 플래그 변수
+flag=False 
+
 # a이상 b이하의 수 중에서 1,920과 2,880의 공약수가 존재하는지 판단
 for n in range(a,b+1):
     if (1920%n==0) and (2880%n==0):
-        print(1)
-        break
-    else:
-        print(0)
-        break
+        flag=True
+
+if flag==True:
+    print(1)
+else:
+    print(0)
