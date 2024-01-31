b8ce441193eb73c8f117a8067a1f1675bf4e5554
Zhyun18 <118049891+Zhyun18@users.noreply.github.com>
2024-01-31 14:16:12 +0900

    Title: 두 사람, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240131/두 사람/two-person.py
+++ 240131/두 사람/two-person.py
@@ -7,7 +7,9 @@ a2=int(a2)
 # 두 사람 중 한 사람이라도 19세 이상이면서 남자면 1 
 # 여집합 개념 사용 -> 둘다 19세 미만, 여자인 경우가 0 나머지는 1
 
-if (a1<19 and 'W' in g1 ) and ( a2<19 and 'W' in g2):
+if a1<19 and a2<19:
+    print(0)
+elif 'W' in g1 and 'W' in g2:
     print(0)
 else:
     print(1)
