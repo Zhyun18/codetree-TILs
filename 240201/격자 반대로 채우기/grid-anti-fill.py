f806d59d7425f7898a3a641da70820064d03c3b9
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-01 22:02:29 +0900

    Title: 격자 반대로 채우기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/격자 반대로 채우기/grid-anti-fill.py
+++ 240201/격자 반대로 채우기/grid-anti-fill.py
@@ -7,16 +7,28 @@ num=1
 
 # 격자 규칙 구현
 for c in range(n-1,-1,-1): # n=4, i: 3,2,1,0
-    if c%2!=0:
+    if n%2==0:
+        if c%2!=0:
         # Case1 
-        for r in range(n-1,-1,-1):
-            arr[r][c]=num 
-            num+=1 
-    else: 
+            for r in range(n-1,-1,-1):
+                arr[r][c]=num 
+                num+=1 
+        else: 
         # Case 2 
-        for r in range(n):
-            arr[r][c]=num 
-            num+=1        
+            for r in range(n):
+                arr[r][c]=num 
+                num+=1        
+    else:
+        if c%2==0:
+        # Case1 
+            for r in range(n-1,-1,-1):
+                arr[r][c]=num 
+                num+=1 
+        else: 
+        # Case 2 
+            for r in range(n):
+                arr[r][c]=num 
+                num+=1       
 
 
 # 격자 출력
