c147e5cc5054783440c6b3c8cb2de53bb2c33b1e
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-10 15:54:28 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240210/특정 규칙에 따른 문자 출력/character-output-according-to-rule.py
@@ -0,0 +1,14 @@
+n=int(input())
+for i in range(1,n+1):
+    for j in range(n-i-1,-1,-1):
+        print(' ',end=' ')
+    for k in range(i):
+        print('@',end=' ')
+    print()
+
+for i in range(n-1,0,-1):
+    for j in range(i):
+        print('@',end=' ')
+    for k in range(n-i+1,i,-1):
+        print(' ',end=' ')
+    print()
