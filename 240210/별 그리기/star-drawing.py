feb42b5e17ba28f1bdc54d14d517f0f4f0511259
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-10 15:45:35 +0900

    Title: 별 그리기, Time: 194ms, Memory: 26MB, Status: Passed - CodeTree

--- 240210/별 그리기/star-drawing.py
+++ 240210/별 그리기/star-drawing.py
@@ -3,7 +3,7 @@ n=int(input())
 # 공백 규칙, 2,1,0...
 
 for i in range(1,n+1): # i=1,2
-    for j in range(n-i,-1,-1):
+    for j in range(n-i-1,-1,-1):
         print('',end=' ')
     for k in range(2*i-1):
         print('*',end='')
@@ -11,7 +11,7 @@ for i in range(1,n+1): # i=1,2
 
 # 반대로
 for i in range(n-1,0,-1): # i: 2,1
-    for j in range(n-i+1): # j: 2.1
+    for j in range(n-i): # j: 2.1
         print('',end=' ')
     for k in range(2*i-1): # k: 3,1 
         print('*',end='')
