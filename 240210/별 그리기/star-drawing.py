ca6e47b4107175d1ee1dbbc7f621706c61922ae8
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-10 15:41:52 +0900

    Title: 별 그리기, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240210/별 그리기/star-drawing.py
@@ -0,0 +1,18 @@
+n=int(input())
+# 별 모양 규칙 : 2n-1, 1,3,5...
+# 공백 규칙, 2,1,0...
+
+for i in range(1,n+1): # i=1,2
+    for j in range(n-i,-1,-1):
+        print('',end=' ')
+    for k in range(2*i-1):
+        print('*',end='')
+    print()
+
+# 반대로
+for i in range(n-1,0,-1): # i: 2,1
+    for j in range(n-i+1): # j: 2.1
+        print('',end=' ')
+    for k in range(2*i-1): # k: 3,1 
+        print('*',end='')
+    print()
