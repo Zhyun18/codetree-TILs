0bf9a7d6cdbe8b3bb1073aa76f4292b1ec723115
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-11 22:16:46 +0900

    Title: 구구단 만들기 2, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240211/구구단 만들기 2/print-multiplication-table-2.py
+++ 240211/구구단 만들기 2/print-multiplication-table-2.py
@@ -1,6 +1,6 @@
 a,b=map(int,input().split())
-for i in range(2,2*b+1,2):
-    for j in range(b,a-1,-1):
+for i in range(2,9,2): # 2 4 6 8
+    for j in range(b,a-1,-1): # a 이상 b 이하 
         print(f'{j} * {i} = {j*i}',end=' ')
         if j>a:
             print('/',end=' ')
