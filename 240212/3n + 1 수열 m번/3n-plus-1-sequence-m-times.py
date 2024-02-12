531ffabc47a2b1c750fd44155d18ed4c4cd29d0d
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-12 14:32:50 +0900

    Title: 3n + 1 수열 m번, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240212/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
+++ 240212/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -1,8 +1,8 @@
 n=int(input())
 # n에서 시작해서 n이 짝수면 2로 나누고, n이 홀수면 3을 곱하고 1을 더해서 n이 1이 되기 전ㄲ싸지 계속 반복
-cnt=0
 for _ in range(n):
     num=int(input())
+    cnt=0  # 초기화 반복 
     while True:
         if num==1:
             break
@@ -13,4 +13,3 @@ for _ in range(n):
             num=num*3+1
             cnt+=1
     print(cnt)
-    cnt=0
