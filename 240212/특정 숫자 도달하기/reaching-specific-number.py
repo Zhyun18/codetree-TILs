69242a65142722808be73a6013d338b9a38f877b
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-12 15:53:20 +0900

    Title: 특정 숫자 도달하기, Time: 132ms, Memory: 72MB, Status: Passed - CodeTree

---
+++ 240212/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,13 @@
+_list=list(map(int,input().split()))
+# 250 이상의 정수가 주어지면, 마지막으로 주어진 수를 제외하고 주어진 모든 정수들의 합계와 평균을 구하는 프로그램
+sum_val=0
+cnt=0
+avg_val=0
+
+for i in range(len(_list)):
+    if _list[i]<250:
+        sum_val+=_list[i]
+        avg_val=sum_val/(i+1)
+    if _list[i]>250:
+        break
+print(f'{sum_val} {avg_val:.1f}')
