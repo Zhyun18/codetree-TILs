59ef8dc60e1d2a9444b34b369c8091e34c8cd1a5
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-13 20:28:49 +0900

    Title: 배열 채우기, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240213/배열 채우기/filling-array.py
@@ -0,0 +1,15 @@
+arr=list(map(int,input().split()))
+ans=[]
+flag=False
+for i in range(len(arr)):
+    if arr[i]==0:
+        flag=True
+    
+if flag==False:
+    ans=arr[i::-1]
+    for j in ans:
+        print(j,end=' ')
+else:
+    ans=arr[i::-1]
+    for j in ans:
+        print(j, end=' ')
