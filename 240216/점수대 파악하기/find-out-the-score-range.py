6bc24be85483e64e3601e0dc9b6362711e6eb9da
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-16 23:31:17 +0900

    Title: 점수대 파악하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240216/점수대 파악하기/find-out-the-score-range.py
+++ 240216/점수대 파악하기/find-out-the-score-range.py
@@ -2,7 +2,9 @@ arr=list(map(int,input().split()))
 count_arr=[0]*101
 for elem in arr: 
     result=(elem//10)*10
-    if result>=10:
+    if elem==0:
+        break
+    elif result>=10:
         count_arr[result]+=1
    
 for i in range(100,9,-10):
