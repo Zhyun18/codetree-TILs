c31ae7e46b4c2646cd8c92131b3967964c57ad01
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-18 12:56:07 +0900

    Title: 특정 위치의 문자, Time: 158ms, Memory: 24MB, Status: Passed - CodeTree

--- 240218/특정 위치의 문자/char-in-specific-location.py
+++ 240218/특정 위치의 문자/char-in-specific-location.py
@@ -1,10 +1,8 @@
 s=input()
 arr=['L','E','B','R','O','S']
-idx='None'
-for i in arr:
-    if s not in arr:
-        print(idx)
-        break
-    else:
-        print(arr.index(s))
-        break
+
+# 굳이 in/not 연산자 사용 반복문을 돌릴 필요가 없다~
+if s not in arr:
+    print('None')
+else:
+    print(arr.index(s))
