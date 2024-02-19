096186ffb64cbd1b0ea01639688167c0205f8ffe
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-19 22:21:26 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240219/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,10 @@
+n=int(input())
+arr=[i for i in list(map(int,input().split()))]
+cnt=0
+answer=0
+for i in arr:
+    if i==2:
+        cnt+=1
+        if cnt==3:
+            answer=i+1
+print(answer)
