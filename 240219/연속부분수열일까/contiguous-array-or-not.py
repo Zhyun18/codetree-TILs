9c1155be40e535a92dadd14d838d6bf52f93b34d
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-19 21:44:47 +0900

    Title: 연속부분수열일까, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240219/연속부분수열일까/contiguous-array-or-not.py
+++ 240219/연속부분수열일까/contiguous-array-or-not.py
@@ -15,9 +15,9 @@ arr_b=[i for i in list(map(int,input().split()))]
 # 연속부분함수임을 판단
 # 끝에 있는 경우 
 flag=False 
-# for i in range(len(arr_a)):
-#     if arr_a[i:len(arr_a)]==arr_b:
-#         flag=True
+for i in range(len(arr_a)):
+    if arr_a[i:len(arr_a)]==arr_b:
+        flag=True
 
 # 중간에 있는 경우 (슬라이싱을하자 min/max 이용)
 idx=[]
@@ -26,10 +26,13 @@ for i in range(len(arr_a)):
         if arr_a[i]==arr_b[j]:
             idx.append(i)
 
-s,e=min(idx),max(idx)
-for i in range(s,e+1):
-    if arr_a[i:e+1]==arr_b:
-        flag=True
+if len(idx)==0:
+    flag=False
+else:
+    s,e=min(idx),max(idx)
+    for i in range(s,e+1):
+        if arr_a[i:e+1]==arr_b:
+            flag=True
 
 if flag==True:
     print('Yes')
