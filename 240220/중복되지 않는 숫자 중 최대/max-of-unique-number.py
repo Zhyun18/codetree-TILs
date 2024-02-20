d5768292011491836d4eb74061cd1df4b18435d0
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-20 20:22:26 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240220/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240220/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -1,19 +1,19 @@
 n=int(input())
 arr=list(map(int,input().split()))
-max_value=arr[0]
+
+
 # 중복하지 않은 숫자 중. 최대값 출력, 존재하지 않는다면 -1 
+# 중복을 어떻게 세면 될까? 
+ans=[]
 for i in arr:
-    if max_value<i:
-        max_value=i
+    if arr.count(i)==1:
+        ans.append(i)
 
-arr.remove(max_value)
-
-flag=True
-for i in arr:
-    if max_value in arr:
-        flag=False
-
-if flag==True:
-    print(max_value)
-else:
+if len(ans)==0:
     print(-1)
+else:
+    max_value=ans[0]
+    for i in ans:
+        if max_value<i:
+            max_value=i 
+    print(max_value)
