29bbf422eba15490dda64e270d9fe299fff00be9
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-20 22:15:18 +0900

    Title: 500 근처의 수, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240220/500 근처의 수/near-500.py
+++ 240220/500 근처의 수/near-500.py
@@ -1,12 +1,11 @@
 arr=list(map(int,input().split()))
+ans1,ans2=[],[]
 #0으로해야할까 배열안에 있는 숫자로 해야할까 고민중.. 
-ans=[]
-for i in range(len(arr)):
-    if arr[i]<500:
-        max_num=arr[i]
-        if max_num<arr[i]:
-            max_num=arr[i] 
-    else:
-        ans.append(arr[i])    
-        min_num=min(ans)
+for i in arr:
+    if i <500:
+        ans1.append(i)
+        max_num=max(ans1)
+    elif i>=500:
+        ans2.append(i)
+        min_num=min(ans2)
 print(max_num,min_num)
