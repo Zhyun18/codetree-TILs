872a8d19683c28ff6995bdaf4c5e761011312ab7
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-20 22:54:08 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240220/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -0,0 +1,19 @@
+n=int(input()) # n년 
+arr=list(map(int,input().split())) # 향후 n년간 자동차 가격 정보 
+
+# 자동차를 단한번 사서 되팔때 최대이익 출력
+# 입찰가 (가격 초기화가 제일 어려운 부분인 것 같다) 
+
+min_value=arr[0]
+idx=0
+for i in range(len(arr)):
+    if min_value>arr[i]:
+        min_value=arr[i] 
+        idx=i+1 
+
+max_value=arr[idx]
+for i in range(idx,len(arr)):
+    if max_value<arr[i]:
+        max_value=arr[i]
+
+print(max_value-min_value)
