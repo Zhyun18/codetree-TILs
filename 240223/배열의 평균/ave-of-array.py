b690cde405ee82421322beff813e661d368270bd
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-23 23:31:23 +0900

    Title: 배열의 평균, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240223/배열의 평균/ave-of-array.py
+++ 240223/배열의 평균/ave-of-array.py
@@ -1,7 +1,7 @@
 arr=[list(map(int, input().split())) for _ in range(2)]
 # 가로평균
 for i in arr:
-    print(sum(i)/len(i),end=' ')
+    print(f'{sum(i)/len(i):.1f}',end=' ')
 print()
 
 # 세로평균
@@ -9,7 +9,7 @@ for i in range(4):
     sum_value=0
     for j in range(2):
         sum_value+=arr[j][i]
-    print(sum_value/2,end=' ')
+    print(f'{sum_value/2:.1f}',end=' ')
 print()
 
 # 전체 평균
@@ -17,4 +17,4 @@ sum_value=0
 for i in range(len(arr)):
     for j in range(len(arr[i])):
         sum_value+=arr[i][j]
-print(sum_value/8)
+print(f'{sum_value/8:.1f}')
