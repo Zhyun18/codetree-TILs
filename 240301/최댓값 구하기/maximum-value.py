5e6cbca83ffffb5e78d1ec2b61b8740cafd68954
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-03-01 00:11:15 +0900

    Title: 최댓값 구하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240301/최댓값 구하기/maximum-value.py
+++ 240301/최댓값 구하기/maximum-value.py
@@ -1,7 +1,7 @@
 a,b,c=tuple(map(int,input().split()))
-if a>b and a>c:
+if a>=b and a>=c:
     print(a)
-elif b>a and b>c:
+elif b>=a and b>=c:
     print(b)
-elif c>a and c>b:
+elif c>=a and c>=b:
     print(c)
