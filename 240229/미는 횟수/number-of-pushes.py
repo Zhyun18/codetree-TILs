c2bfddc52d2c63349c4c781053992d3d655392ec
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-29 14:17:44 +0900

    Title: 미는 횟수, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240229/미는 횟수/number-of-pushes.py
@@ -0,0 +1,17 @@
+a=input()
+b=input()
+arr=list(a)
+
+cnt=0
+flag=False
+for _ in range(len(arr)):
+    arr=arr[1:]+arr[0:1]
+    cnt+=1
+    if arr==list(b):
+        flag=True
+        break
+
+if flag==True:
+    print(cnt)
+else:
+    print(-1)
