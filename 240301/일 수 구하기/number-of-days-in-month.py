6281971ef540e6adbe227dc7577e487a83e100c4
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-03-01 00:09:34 +0900

    Title: 일 수 구하기, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240301/일 수 구하기/number-of-days-in-month.py
+++ 240301/일 수 구하기/number-of-days-in-month.py
@@ -1,7 +1,8 @@
 n=int(input())
 if n==2:
     print(28)
-if n in [1,3,5,7,8,10,12]:
-    print(31)
 else:
-    print(30)
+    if n in [1,3,5,7,8,10,12]:
+        print(31)
+    else:
+        print(30)
