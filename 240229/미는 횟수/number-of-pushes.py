4b55adc1b87b0332f5aede59e5c3ce4eb991ab7b
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-02-29 15:09:54 +0900

    Title: 미는 횟수, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240229/미는 횟수/number-of-pushes.py
+++ 240229/미는 횟수/number-of-pushes.py
@@ -5,8 +5,8 @@ arr=list(a)
 cnt=0
 flag=False
 for i in range(len(arr)):
-    arr=arr[-1:]+arr[0:i-1]
-    print(arr)
+    arr=arr[-1:]+arr[0:len(arr)-1]
+    cnt+=1
     if arr==list(b):
         flag=True
         break
