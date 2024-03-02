324744a6fda09b62e21e6fcfbd9d4021b58db823
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-03-02 23:29:47 +0900

    Title: 문자열 계속 지우기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240302/문자열 계속 지우기/keep-removing-string.py
+++ 240302/문자열 계속 지우기/keep-removing-string.py
@@ -5,9 +5,8 @@ arr=list(a)
 
 while True:
     if b not in arr:
+        print(''.join(arr))
         break
     else:
         idx=arr.index(b)
         arr=arr[:idx]+arr[idx+1:]
-
-print(''.join(arr))
