df2e6a8768c5e76278ce4507a76880b7ddaf0888
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-03-04 20:51:09 +0900

    Title: 최대공약수 구하기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240304/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,7 +1,7 @@
 n,m=tuple(map(int,input().split()))
 def solution(n,m):
     ans=[]
-    for i in range(1,100):
+    for i in range(1,100+1): # 100까지 포함해야지
         if n%i==0 and m%i==0:
             ans.append(i)
     print(max(ans))
