a835ec71f4fa41d09ac733f6c11b3f2be4a48ea8
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-03-04 21:10:08 +0900

    Title: 최소공배수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240304/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240304/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,9 +1,8 @@
 n,m=tuple(map(int,input().split()))
 def solution(n,m):
-    ans=0
+    ans=[]
     for i in range(1,101):
         if i%n==0 and i%m==0:
-            ans=i
-            break
-    print(ans)
+            ans.append(i)
+    print(min(ans))
 solution(n,m)
