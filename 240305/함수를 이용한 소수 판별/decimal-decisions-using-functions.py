1a8685ac5b2511e262eb1e1b5dfeaf02af75faff
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-03-05 22:11:52 +0900

    Title: 함수를 이용한 소수 판별, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240305/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,11 @@
+a,b=tuple(map(int,input().split()))
+def solution(n):
+    for i in range(2,n):
+        if n%i==0:
+            return 0
+    return n # 위치 
+
+sum_value=0
+for i in range(a,b+1):
+    sum_value+=solution(i)
+print(sum_value)
