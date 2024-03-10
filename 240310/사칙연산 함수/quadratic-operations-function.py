9f65610b20c5ce1111907e7fc54007578a5910e8
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-03-10 09:03:19 +0900

    Title: 사칙연산 함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240310/사칙연산 함수/quadratic-operations-function.py
+++ 240310/사칙연산 함수/quadratic-operations-function.py
@@ -1,7 +1,7 @@
 a,b,c=tuple(input().split())
 def solution(a,b,c):
     if b not in ['+','-','/','*']:
-        return false
+        return False $ false 는 대문자로 쓰세요;; 
     else:
         if b=='+':
             return int(a)+int(c)
