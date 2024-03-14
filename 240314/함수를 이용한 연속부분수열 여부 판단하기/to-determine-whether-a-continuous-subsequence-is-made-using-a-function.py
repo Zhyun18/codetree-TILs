5daf74a972d4213824906ff202aa6e4650d48da2
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-03-14 23:27:52 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240314/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,37 @@
+n1,n2=tuple(map(int,input().split()))
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+# 수열 B가 수열 A의 연속부분 수열인지 판단
+def solution(a,b):
+    start=0
+    end=0
+    for i in range(len(a)):
+        if b[0]==b[1]:
+            b=b[1:]
+        if b[0]==a[i]:
+            start=i
+        if b[-1]==a[i]:
+            end=i
+    if a[start:end+1]==b:
+        return True  
+    else:
+        return False
+        
+
+if solution(a,b):
+    print('Yes')
+else:
+    print('No')
+
+# start=0
+# end=0
+# for i in range(len(a)):
+#     if b[0]==b[1]:
+#         b=b[1:]
+#     if b[0]==a[i]:
+#         start=i
+#     if b[-1]==a[i]:
+#         end=i
+# print(a[start:end+1])
+# print(b)
