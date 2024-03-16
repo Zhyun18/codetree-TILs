596a4b002fd4607f62c0b84794119bd27da0bf27
JeeheeHyun <118049891+Zhyun18@users.noreply.github.com>
2024-03-16 23:27:17 +0900

    Title: 2개 이상의 알파벳, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240316/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240316/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,10 +1,12 @@
 a=input()
 def solution(s):
     string=a[0]
+    cnt=0
     for i in range(1,len(a)):
         if a[i]!=string:
-            return 'Yes'
-        else:
-            return 'No'
-
+            cnt+=1
+    if cnt>=2:
+        return "Yes"
+    else:
+        return "No"
 print(solution(a))
