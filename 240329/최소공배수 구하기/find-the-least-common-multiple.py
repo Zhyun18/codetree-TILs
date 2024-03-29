89e9c2a7ca54c6f58fc65760b6683da000420638
코딩하는_딥상어동 <118049891+Zhyun18@users.noreply.github.com>
2024-03-29 23:38:18 +0900

    Title: 최소공배수 구하기, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240329/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240329/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,8 +1,10 @@
-n,m=input().split()
-n,m=int(n),int(m)
-num=0
-for i in range(2,n):
-    if n%i==0 and m%i==0 and i*n%m==0: # 최대공약수
-        num=i
-        print(num*n) 
-        break
+n,m=tuple(map(int,input().split()))
+
+def solution(n,m):
+    num=0
+    for i in range(1,min(n,m)+1):
+        if n%i==0 and m%i==0:
+            num=i 
+    print(n*m//num)
+
+solution(n,m)
