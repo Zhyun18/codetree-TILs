b93a90f9faed211a7fcafdc448262c87aaba574d
코딩하는_딥상어동 <118049891+Zhyun18@users.noreply.github.com>
2024-05-21 23:50:07 +0900

    Title: k번째로 신기한 문자열, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240521/k번째로 신기한 문자열/kth-special-string.py
+++ 240521/k번째로 신기한 문자열/kth-special-string.py
@@ -3,10 +3,11 @@ n,k,t=tuple(input().split())
 n,k=int(n),int(k)
 
 # t로 시작하는 단어 중에 사전순으로 정렬했을 때 k 번째 문자열 출력 
-# 빈 리스트 
+# 빈 리스트
+num=len(t) 
 my_list=[]
 for _ in range(n):
     s=input()
-    if t in s: 
+    if s[:num]==t: 
         my_list.append(s) 
 print(sorted(my_list)[k-1])
